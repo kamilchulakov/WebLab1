b8901f65ae00c7442981ef05d749106f4f9e4076
ulyanovskk <mr1english4@gmail.com>
2021-09-02 14:27:17 +0300

    Removed bad code :|

--- scripts/main.php
+++ scripts/main.php
@@ -31,49 +31,6 @@ function atArea($x, $y, $r)
     else return "outside";
 }
 
-function json_encode_for_helios($a = false)
-{
-    if (is_null($a)) {
-        return 'null';
-    }
-    if ($a === false) {
-        return 'false';
-    }
-    if ($a === true) {
-        return 'true';
-    }
-    if (is_scalar($a)) {
-        if (is_float($a)) {
-            return (float)str_replace(',', '.', (string)$a);
-        }
-        if (is_string($a)) {
-            static $jsonReplaces = array(array('\\', '/', "\n", "\t", "\r", "\b", "\f", '"'), array('\\\\', '\\/', '\\n', '\\t', '\\r', '\\b', '\\f', '\"'));
-            return '"' . str_replace($jsonReplaces[0], $jsonReplaces[1], $a) . '"';
-        }
-
-        return $a;
-    }
-    for ($i = 0, reset($a); true; $i++) {
-        if (key($a) !== $i) {
-            $isList = false;
-            break;
-        }
-    }
-
-    $result = array();
-    if ($isList) {
-        foreach ($a as $v) {
-            $result[] = json_encode_for_helios($v);
-        }
-        return '[' . implode(',', $result) . ']';
-    }
-
-    foreach ($a as $k => $v) {
-        $result[] = json_encode_for_helios((string)$k) . ':' . json_encode_for_helios($v);
-    }
-    return '{' . implode(',', $result) . '}';
-}
-
 session_start();
 date_default_timezone_set('Europe/Moscow');
 
@@ -102,5 +59,6 @@ $_SESSION["tableRows"][] = array(
     'currentTime' => $currentTime,
     'benchmarkTime' => $benchmarkTime
 );
-echo json_encode_for_helios($_SESSION["tableRows"]);
+//echo json_encode_for_helios($_SESSION["tableRows"]);
+echo json_encode($_SESSION["tableRows"]);
 
